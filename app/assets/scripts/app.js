d12c10abcbf6e3fbe131035253f6b57dfa36b5e2
rodrigovallades <rodrigo.vallades@gmail.com>
2018-03-19 22:45:16 -0300

    Fetching GraphQL api

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,5 +1,6 @@
-import GoogleMaps from './modules/GoogleMaps'
-import Loader from './modules/Loader'
+import GoogleMaps from './modules/GoogleMaps';
+import Loader from './modules/Loader';
+import { createApolloFetch } from 'apollo-fetch';
 
 const gMap = new GoogleMaps()
 const loader = new Loader()
@@ -23,17 +24,97 @@ class BeerLocator {
         .then(res => res.json())
         .then(data => {
           loader.unblock();
-          const location = data.results[0].geometry.location;
-          console.log(data.results);
+          this.address = data.results[0];
+
+          // add marker to gMap
+          const location = this.address.geometry.location;
           gMap.addMarkers([{lat: location.lat, lng: location.lng}])
 
-          this.address = data.results[0];
-          this.renderAddress();
+          this.renderAddress(this.address.formatted_address);
+
+          // fetch GraphQL api
+          this.getPOC(location);
+
+          console.log(`Google maps API:`);
+          console.log(data.results);
         })
         .catch()
     } else if (e.key === 'Escape') {
       this.clear(input)
     }
+  }
+
+  getPOC(location) {
+    loader.block();
+    const fetch = createApolloFetch({
+      uri: 'https://803votn6w7.execute-api.us-west-2.amazonaws.com/dev/public/graphql',
+    });
+
+    const date = new Date();
+    date.toISOString();
+
+    fetch({
+      query: `query pocSearchMethod($now: DateTime!, $algorithm: String!, $lat: String!, $long: String!) {
+        pocSearch(now: $now, algorithm: $algorithm, lat: $lat, long: $long) {
+          __typename
+          id
+          status
+          tradingName
+          officialName
+          deliveryTypes {
+            __typename
+            pocDeliveryTypeId
+            deliveryTypeId
+            price
+            title
+            subtitle
+            active
+          }
+          paymentMethods {
+            __typename
+            pocPaymentMethodId
+            paymentMethodId
+            active
+            title
+            subtitle
+          }
+          pocWorkDay {
+            __typename
+            weekDay
+            active
+            workingInterval {
+              __typename
+              openingTime
+              closingTime
+            }
+          }
+          address {
+            __typename
+            address1
+            address2
+            number
+            city
+            province
+            zip
+            coordinates
+          }
+          phone {
+            __typename
+            phoneNumber
+          }
+        }
+      }`,
+      variables: {
+        "algorithm": "NEAREST",
+        "lat": location.lat,
+        "long": location.lng,
+        "now": date
+      },
+    }).then(res => {
+      loader.unblock();
+      console.log(`GraphQL POC:`);
+      console.log(res.data);
+    });
   }
 
   clear(input) {
@@ -42,11 +123,11 @@ class BeerLocator {
     gMap.initBrazil()
   }
 
-  renderAddress() {
+  renderAddress(address) {
     const addressResults = document.querySelector('.address__results')
     addressResults.innerHTML = `
       <ul class='address__list'>
-        <li class='address__list-item'>${this.address.formatted_address}</li>
+        <li class='address__list-item'>${address}</li>
       </ul>
     `
   }
